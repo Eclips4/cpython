c45e5468ace5a79552c1a44f86fc2482a57fd0c8
Kirill Podoprigora <kirill.bast9@mail.ru>
2024-12-21 21:53:31 +0200

    remove unnecessary change

--- Python/codegen.c
+++ Python/codegen.c
@@ -1675,9 +1675,6 @@ codegen_typealias(compiler *c, stmt_ty s)
 static bool
 check_is_arg(expr_ty e)
 {
-    if (e->kind == Tuple_kind) {
-        return false;
-    }
     if (e->kind != Constant_kind) {
         return true;
     }
